3dc2faaa24c8f902b73e146e8689c7a259001c73
Petar Virijević <wiriyevich@gmail.com>
2024-12-27 12:24:00 +0100

    initial commit

---
+++ src/main/java/com/wirke/java_spring_modulith/JavaSpringModulithApplication.java
@@ -0,0 +1,13 @@
+package com.wirke.java_spring_modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wirke/java_spring_modulith/JavaSpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirke.java_spring_modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
