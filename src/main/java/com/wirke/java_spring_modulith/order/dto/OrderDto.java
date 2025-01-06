b17f07b709f00c96d179e02daf9578f5829560ee
Petar Virijević <wiriyevich@gmail.com>
2025-01-06 12:14:38 +0100

    added DTOs for ordering

---
+++ src/main/java/com/wirke/java_spring_modulith/order/dto/OrderDto.java
@@ -0,0 +1,7 @@
+package com.wirke.java_spring_modulith.order.dto;
+
+import java.util.List;
+
+public record OrderDto (String customerName,
+                        String CustomerEmail,
+                        List<InventoryRequestDto> InventoryRequestDtoList){}
