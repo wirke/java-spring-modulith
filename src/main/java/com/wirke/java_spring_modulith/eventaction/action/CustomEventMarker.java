ad21c49f1106466354f4188e757e61e0e404a7f2
Petar Virijević <wiriyevich@gmail.com>
2025-01-08 15:53:18 +0100

    created action dependency via @interface

---
+++ src/main/java/com/wirke/java_spring_modulith/eventaction/action/CustomEventMarker.java
@@ -0,0 +1,15 @@
+package com.wirke.java_spring_modulith.eventaction.action;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomEventMarker {
+    
+    Action eventAction();
+}
