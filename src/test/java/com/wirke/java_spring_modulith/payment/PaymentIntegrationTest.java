851e790cede27e3b6e92b2f04a30f10f2ebca561
Petar Virijević <wiriyevich@gmail.com>
2025-01-08 12:52:02 +0100

    Create PaymentIntegrationTest.java

---
+++ src/test/java/com/wirke/java_spring_modulith/payment/PaymentIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.wirke.java_spring_modulith.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+@ApplicationModuleTest
+public class PaymentIntegrationTest {
+
+    @Test
+    void verify(){
+        
+    }
+}
