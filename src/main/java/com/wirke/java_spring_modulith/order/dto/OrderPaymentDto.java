39be9d3ba5e1f4ed8afd1c919d791cac5c87c2fd
Petar Virijević <wiriyevich@gmail.com>
2025-01-08 15:52:23 +0100

    implemented action type PAYMENT

--- src/main/java/com/wirke/java_spring_modulith/order/dto/OrderPaymentDto.java
+++ src/main/java/com/wirke/java_spring_modulith/order/dto/OrderPaymentDto.java
@@ -2,4 +2,8 @@ package com.wirke.java_spring_modulith.order.dto;
 
 import org.jmolecules.event.types.DomainEvent;
 
+import com.wirke.java_spring_modulith.eventaction.action.Action;
+import com.wirke.java_spring_modulith.eventaction.action.CustomEventMarker;
+
+@CustomEventMarker(eventAction = Action.PAYMENT)
 public record OrderPaymentDto (String orderId, long amount) implements DomainEvent {}
