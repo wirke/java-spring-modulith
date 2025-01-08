64fd56dc5c6fd60616604b7eac7ec52690edd135
Petar Virijević <wiriyevich@gmail.com>
2025-01-08 12:56:38 +0100

    package-info added

---
+++ src/main/java/com/wirke/java_spring_modulith/order/dto/package-info.java
@@ -0,0 +1,2 @@
+@org.springframework.modulith.NamedInterface("dto")
+package com.wirke.java_spring_modulith.order.dto;
---
+++ src/main/java/com/wirke/java_spring_modulith/payment/package-info.java
@@ -0,0 +1,4 @@
+@org.springframework.modulith.ApplicationModule(
+    allowedDependencies = {"order::dto"}
+)
+package com.wirke.java_spring_modulith.payment;
