132ed1272e0dd5842693976c2e428cdee4987bcb
Petar Virijević <wiriyevich@gmail.com>
2025-01-08 15:51:24 +0100

    added action dependency in order package info

--- src/main/java/com/wirke/java_spring_modulith/order/package-info.java
+++ src/main/java/com/wirke/java_spring_modulith/order/package-info.java
@@ -1,4 +1,4 @@
 @org.springframework.modulith.ApplicationModule(
-    allowedDependencies = {"inventory::exposed"}
+    allowedDependencies = {"inventory::exposed", "eventaction::action"}
 )
 package com.wirke.java_spring_modulith.order;
